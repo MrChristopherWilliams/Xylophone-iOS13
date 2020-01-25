3a1d08f157c68b1e91c6745c530887c1c759a6d5
Chris Williams <chriswilliams14.williams@gmail.com>
2020-01-25 18:14:28 +0000

    Added fade function

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -19,13 +19,17 @@ class ViewController: UIViewController {
 
     @IBAction func keyPressed(_ sender: UIButton) {
         playSound(soundName: sender.currentTitle!)
+        sender.alpha = 0.2
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+        sender.alpha = 1.0
+        }
+        
     }
     
     func playSound(soundName: String) {
         let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
-                
     }
 }
 
